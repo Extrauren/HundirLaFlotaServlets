c9eb5e881d8770875afdd0b957735c24fc699b39
Dani <Dani@DESKTOP-RT3GMCH>
2018-12-11 11:41:45 +0100

    Servlet de prueba casilla finalizado y tablero creado pero sin hacer

--- HundirLaFlotaServlet/src/controlador/HundirLaFlotaServlet.java
+++ HundirLaFlotaServlet/src/controlador/HundirLaFlotaServlet.java
@@ -1,10 +1,15 @@
 package controlador;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelo.Partida;
 
 /**
  * Servlet implementation class HundirLaFlotaServlet
@@ -23,17 +28,54 @@ public class HundirLaFlotaServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
+    
+    
+    /**Cargar la sesion o crearla si no existe
+    *Si la sesion no contiene el atributo ’partida’
+    *Crear la partida
+    *sino
+    *Obtener la partida en juego de la sesion
+    *Obtener de la peticion la posicion del radio button pulsado
+    *Usar el metodo del objeto Partida para disparar sobre la casilla elegida
+    *Guardar el objeto Partida en la sesion
+    *Redirigir la ejecucion al fichero JSP que construira la Vista a mostrar al usuario
+    */
+    
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		response.setContentType("text/hyml");    			//marcamos el tipo del contenido
+		
+		HttpSession sesion = request.getSession(true);
+		Partida partida = null;
+		boolean marcada = false;
+		
+		
+		if(sesion.getAttribute("partida")==null) {		
+			
+			partida = new Partida(8,8,6);			
+			
+		}else {												
+			
+			partida = (Partida) sesion.getAttribute("partida");		
+			if(request.getParameter("casilla")!=null) {
+				String coordenadas[] = request.getParameter("casilla").split("#"); 
+				int fila = Integer.parseInt(coordenadas[0]);
+				int col =  Integer.parseInt(coordenadas[1]);
+				marcada = partida.casillaDisparada(fila, col);					
+				partida.pruebaCasilla(fila, col);									
+			}	
+			
+		}
+		
+		sesion.setAttribute("partida", partida);		
+		request.setAttribute("marcada", marcada);			
+		
+		RequestDispatcher vista = request.getRequestDispatcher("TableroLocal.jsp");
+		vista.forward(request, response);							
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
